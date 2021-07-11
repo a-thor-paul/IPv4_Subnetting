e69717e42232426b636183c93c3f192d2f1a34eb
a-thor-paul <thor.paul.4@gmail.com>
2021-07-11 18:30:48 -0400

    First commit with comments outlining project

---
+++ ipv4_sn.py
@@ -0,0 +1,5 @@
+#This program will allow the user to input IPv4 data such as:
+#Network Bits, Subnet Mask, Bits Borrowed, total Subnets, 
+#and total Hosts to get the full Subnet Information.
+#The use of this program is for when only partial subnet information
+#has been provided to the user.
