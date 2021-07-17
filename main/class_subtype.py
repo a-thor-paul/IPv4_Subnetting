b471c369f43f21f15c186a70555c86ecf925c993
a-thor-paul <thor.paul.4@gmail.com>
2021-07-16 22:46:37 -0400

    altered class_subtype.py

--- main/class_subtype.py
+++ main/class_subtype.py
@@ -5,7 +5,19 @@ def class_st_check():
     print("What Class Subnet are you dealing with? \nCurrently only Class A, B and C are supported.")
     class_type = input("Class: ")
     if(re.search(class_list, class_type)):
-        print("You have chosen Class: {}".format(class_type))
+        class_a()
     else:
         print("nope, try again")
         class_st_check()
+
+def class_a():
+    print("You have chosen Class A! \nWhat data would you like to search with, to find your remaining subnet information? \nChoose between: Network Bits(NB), Subnet Mask(SM), Bits Borrowed(BB), Subnets(SN), or Number of Hosts(NH).")
+    cl_a = "^NB$|^nb$|^SM$|^sm$|^BB$|^bb$|^SN$|^sn$|^NH$|^nh$"
+    user_in = input()
+    if(re.search(cl_a, user_in)):
+        print("You selected: {}".format(user_in))
+        
+    else:
+        print("Try again!")
+        class_a()
+
