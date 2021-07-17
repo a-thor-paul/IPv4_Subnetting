a4377899b2d6faa48c4a4b2302de18486dd9bdbe
a-thor-paul <thor.paul.4@gmail.com>
2021-07-17 14:49:34 -0400

    added more functionality with dictionaries and user input

--- main/class_subtype.py
+++ main/class_subtype.py
@@ -1,29 +1,48 @@
 import re
 
-def class_st_check():
+def class_subtype_check():
     
+    class_subtype_dict = {
+        "A" : class_a,
+        "B" : class_a,
+        "C" : class_a,
+    }
+
     #regex list to confirm option A, B or C is selected in lower/uppercase
     class_list = "^A$|^B$|^C$"
    
     print("\nWhat Class Subnet are you dealing with? \nCurrently only Class A, B and C are supported.")
     
-    class_type = input("\nClass: ").upper()
+    user_in = input("\nClass: ").upper()
     
     #regex search to confirm user input is in regex list
-    if(re.search(class_list, class_type)):
-        class_a()
+    if(re.search(class_list, user_in)):
+        
+        print("\nYou have chosen Class {}! \n\nWhat data would you like to search with, to find your remaining subnet information?\nChoose one of the following options: \n - Network Bits (NB)\n - Subnet Mask (SM)\n - Bits Borrowed (BB)\n - Subnets (SN)\n - Number of Hosts (NH)".format(user_in))
+
+        class_subtype_dict[user_in]()
+
     else:
         print("nope, try again")
-        class_st_check()
-
-
-
+        class_subtype_check()
 
 def net_bit():
     print("It worked: Network bits")
 
 def sub_mask():
-    print("It worked: Subnet Mask")
+
+    #print("It worked: Subnet Mask")
+    
+    #Regex for Subnet Mask format xxx.xxx.xxx.xxx
+    ip_add = "[0-9]\d{0,3}\.[0-9]\d{0,3}\.[0-9]\d{0,3}\.[0-9]\d{0,3}"
+
+    user_in = input("Type your subnet mask: ")
+
+    if(re.search(ip_add, user_in)):
+        print(user_in)
+
+    else:
+        print("gottem")
 
 def bit_brw():
     print("It worked: Bits Borrowed")
@@ -36,7 +55,7 @@ def num_host():
 
 class_dict = {
     "NB" : net_bit,
-    "SM" :sub_mask,
+    "SM" : sub_mask,
     "BB" : bit_brw,
     "SN" : sub_nets,
     "NH" : num_host,
@@ -44,16 +63,14 @@ class_dict = {
 
 def class_a():
     
-    print("\nYou have chosen Class A! \n\nWhat data would you like to search with, to find your remaining subnet information?\nChoose one of the following options: \n - Network Bits (NB)\n - Subnet Mask (SM)\n - Bits Borrowed (BB)\n - Subnets (SN)\n - Number of Hosts (NH)")
-    
     #regex list to check if input matches options in lower or upper case
-    cl_a = "^NB$|^nb$|^SM$|^sm$|^BB$|^bb$|^SN$|^sn$|^NH$|^nh$"
+    cl_a = "^NB$|^SM$|^BB$|^SN$|^NH$"
     
     user_in = input("\nSelect an option: ").upper()
 
     #regex search to confirm user input is in regex list
     if(re.search(cl_a, user_in)):
-        #print("\nYou selected: {}".format(user_in))
+
         class_dict[user_in]()
         
     else:
--- main/ipv4_sn.py
+++ main/ipv4_sn.py
@@ -1,17 +1,17 @@
-#This program will allow the user to input IPv4 data such as:
-#Network Bits, Subnet Mask, Bits Borrowed, total Subnets, 
-#and total Hosts to get the full Subnet Information.
-#The use of this program is for when only partial subnet information
-#has been provided to the user.
+# This program will allow the user to input IPv4 data such as:
+# Network Bits, Subnet Mask, Bits Borrowed, total Subnets,
+# and total Hosts to get the full Subnet Information.
+# The use of this program is for when only partial subnet information
+# has been provided to the user.
 
 import re
 import class_subtype
 
-class_subtype.class_st_check()
-#Regex for IP format xxx.xxx.xxx
+class_subtype.class_subtype_check()
+# Regex for IP format xxx.xxx.xxx
 #ip_add = "[0-9]\d{1,3}\.[0-9]\d{1,3}\.[0-9]\d{1,3}"
 
-#if (re.search(ip_add, class_type)):
+# if (re.search(ip_add, class_type)):
 #    print(class_type)
-#else:
+# else:
 #    print("nope")
